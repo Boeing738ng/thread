426f5c435feb68180d154b5a2989e2e01c0c7c1c
BOEING <BOEING@BOEING738NGs-MacBook-Pro.local>
2015-01-21 21:53:12 +0800

    Initial Commit

---
+++ thread/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  thread
+//
+//  Created by BOEING on 1/21/15.
+//  Copyright (c) 2015 BOEING. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
